42dcce696f362d9883b63f5533bce2ffd5fd214c
Harilanto <Raharisonhfananmbinantsoa@gmail.com>
2026-08-05 16:48:23 +0300

    refactor: update round model, handle split scores as a single round

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -26,19 +26,15 @@ class Game {
   }
 
   int _totalScore(TeamType teamType) {
-    return rounds
-        .where((round) => round.winnerTeam == teamType)
-        //where: équivalent à filter dans typescript
-        .map((round) => round.score)
-        .fold(0, (total, score) => total + score);
-
-    //fold, accumulateur equivalent à reduce()
-    //argument1: 0 valeur de départ => total=0
-    //argument2: fonction qui prend en paramète l'accumulateur total et l'élément courant de la liste
-  }
-
-  List<Round> _groupRoundsByTeam(TeamType teamType) {
-    return rounds.where((r) => r.winnerTeam == teamType).toList();
+    return rounds.fold(
+      //fold, accumulateur equivalent à reduce()
+      //argument1: 0 valeur de départ => total=0
+      //argument2: fonction qui prend en paramète l'accumulateur total et l'élément courant de la liste
+      0,
+      (total, round) =>
+          total +
+          (teamType == TeamType.teamA ? round.teamAScore : round.teamBScore),
+    );
   }
 
   int get totalScoreA => _totalScore(TeamType.teamA);
@@ -46,8 +42,6 @@ class Game {
   //on pourrait ecire game.totalScoreB pour récupérer le score total de l'équipe B au lieu de game.totalScoreB() comme une méthode.
 
   int get totalScoreB => _totalScore(TeamType.teamB);
-  List<Round> get teamARounds => _groupRoundsByTeam(TeamType.teamA);
-  List<Round> get teamBRounds => _groupRoundsByTeam(TeamType.teamB);
   List<Team> get teams => [teamA, teamB];
 
   void finishGame(TeamType winnerTeam) {
