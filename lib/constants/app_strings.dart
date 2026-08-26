d909b5532bedb8c70bc679f80b6c6c6c82f8659e
Harilanto <Raharisonhfananmbinantsoa@gmail.com>
2026-08-26 14:08:56 +0300

    refactor : gameVariant and roundStatus abbreviations strings

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -45,14 +45,6 @@ class AppStrings {
   static const String trophyEmoji = '🏆';
   static const String cardEmoji = '🃏';
 
-  static const String clubsSymbol = '♣';
-  static const String diamondsSymbol = '♦';
-  static const String heartsSymbol = '♥';
-  static const String spadesSymbol = '♠';
-  static const String allTrumpAbbreviation = "TA";
-  static const String noTrumpAbbreviation = "SA";
-  static const String doubledAbbreviation = "x2";
-  static const String redoubledAbbreviation = "x4";
   static const String dedansAbbreviation = "D";
   static const String capotAbbreviation = "Cap.";
 
--- lib/enums/game_variant.dart
+++ lib/enums/game_variant.dart
@@ -1,22 +1,18 @@
-import 'package:score_belote/constants/app_strings.dart';
-
 enum GameVariant {
-  clubs("Trèfle", AppStrings.clubsSymbol, 32, 50, 70),
-  diamonds("Carreau", AppStrings.diamondsSymbol, 16, 25, 35),
-  hearts("Cœur", AppStrings.heartsSymbol, 16, 25, 35),
-  spades("Pique", AppStrings.spadesSymbol, 16, 25, 35),
-  noTrump("SansA", AppStrings.noTrumpAbbreviation, 52, 70, 90),
-  allTrump("ToutA", AppStrings.allTrumpAbbreviation, 26, 35, 45);
+  clubs("Trèfle", 32, 50, 70),
+  diamonds("Carreau", 16, 25, 35),
+  hearts("Cœur", 16, 25, 35),
+  spades("Pique", 16, 25, 35),
+  noTrump("SansA", 52, 70, 90),
+  allTrump("ToutA", 26, 35, 45);
 
   final String label;
   final int baseScore;
   final int capotScore;
   final int capotDedansScore;
-  final String abbreviation;
 
   const GameVariant(
     this.label,
-    this.abbreviation,
     this.baseScore,
     this.capotScore,
     this.capotDedansScore,
@@ -28,4 +24,21 @@ enum GameVariant {
       this != GameVariant.allTrump && this != GameVariant.noTrump;
 
   String get suitSymbol => isSuit ? abbreviation : "";
+
+  String get abbreviation {
+    switch (this) {
+      case GameVariant.clubs:
+        return '♣';
+      case GameVariant.diamonds:
+        return '♦';
+      case GameVariant.hearts:
+        return '♥';
+      case GameVariant.spades:
+        return '♠';
+      case GameVariant.noTrump:
+        return 'SA';
+      default:
+        return 'TA';
+    }
+  }
 }
